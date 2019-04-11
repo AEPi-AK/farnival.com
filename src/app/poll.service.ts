5c78055b045e1e04bbb79377c0dd34d9f7d20a62
Avi Romanoff <avi@romanoff.me>
2019-04-10 22:39:50 -0700

    use relative hostname for polling endpoint, add run script

--- src/app/poll.service.ts
+++ src/app/poll.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 import { PollResult } from './poll-result';
@@ -11,16 +11,18 @@ import { RESULTS } from './mock-poll-results'
 export class PollService {
   constructor(private http: HttpClient) { }
 
-  private pollGetUrl = 'http://127.0.0.1:5000/votes';
-  private pollPostUrl = 'http://127.0.0.1:5000/vote_';
+  private pollURL = `http://${window.location.hostname}:5000`;
+
+  private pollGetUrl = `${this.pollURL}/votes`;
+  private pollPostUrl = `${this.pollURL}/vote_`;
 
   getResults(): Observable<PollResult> {
-  	var r = this.http.get<PollResult>(this.pollGetUrl);
+    var r = this.http.get<PollResult>(this.pollGetUrl);
     return r;
   }
 
   updateResults(person: string) {
     console.log("updating");
-  	this.http.post<PollResult>(this.pollPostUrl+person,"").subscribe();
+    this.http.post<PollResult>(this.pollPostUrl + person, "").subscribe();
   }
 }
