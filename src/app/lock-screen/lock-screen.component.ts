077372258e77663e59e4e2e65d56d7931332d0e6
Avi Romanoff <avi@romanoff.me>
2019-04-11 09:31:00 -0700

    tweak

--- src/app/lock-screen/lock-screen.component.ts
+++ src/app/lock-screen/lock-screen.component.ts
@@ -20,8 +20,7 @@ export class LockScreenComponent implements OnInit {
 
   submit() {
     this.authenticationService.checkAuth(this.guess);
-    this.has_failed = true;
-    // this.has_failed = this.authenticationService.unlocked.auth;
+    this.has_failed = this.authenticationService.unlocked.auth;
   }
 
 }
